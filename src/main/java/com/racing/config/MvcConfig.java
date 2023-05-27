91e089996f7cb835dba26dd28941f1ded2acac19
djhelbert <djhelbert@gmail.com>
2023-05-27 08:35:38 -0700

    Added security

---
+++ src/main/java/com/racing/config/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.racing.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index").setViewName("index");
+        registry.addViewController("/about").setViewName("about");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
